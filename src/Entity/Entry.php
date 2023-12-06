a72b89abc9960abee456e30eeb63a08fb3cabd3f
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-12-06 13:21:17 +0100

    created new entity time machine entry

---
+++ src/Entity/Entry.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EntryRepository::class)]
+class Entry
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 1024)]
+    private ?string $resourceUrl = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getResourceUrl(): ?string
+    {
+        return $this->resourceUrl;
+    }
+
+    public function setResourceUrl(string $resourceUrl): static
+    {
+        $this->resourceUrl = $resourceUrl;
+
+        return $this;
+    }
+}
